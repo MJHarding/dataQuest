d0aa86ae53fe0152567f069fc35aeb37f5932f09
Matt HArding <mattm08@live.co.uk>
2020-02-21 10:46:18 +0000

    Second com

--- landCalc.py
+++ landCalc.py
@@ -8,3 +8,28 @@ Created on Fri Feb 21 09:45:23 2020
 
 """
 
+import numpy as np
+import csv
+
+oilCSV = "Oil.csv"
+wheatCSV = "Wheat.csv"
+goldCSV = "Gold.csv"
+ironCSV =  "Iron.csv"
+
+reader = csv.reader(open("CsvExample.csv"))
+
+
+
+
+
+"""  
+TODO 
+Parse values over time 
+"""
+
+
+
+""" 
+TODO 
+Algorithm to select land
+"""
